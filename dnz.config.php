4945d7e8393966af580243c38b41ddd0d7d9c907
andreadanzi <andrea@danzi.tn.it>
2017-07-05 15:43:40 +0200

    Fix #9 - mapping between projecttype and project template name

---
+++ dnz.config.php
@@ -0,0 +1,7 @@
+<?php
+// danzi.tn#9 - 20170705 - mapping between projecttype and template name for project creation based on template
+$dnzProjectTypeMap = array(
+    'Coflyer' => 'Template RC',
+);
+
+?>
--- modules/com_vtiger_workflow/custom_wf_projects.inc.php
+++ modules/com_vtiger_workflow/custom_wf_projects.inc.php
@@ -1,11 +1,14 @@
 <?php
 require_once('include/database/PearDatabase.php');
 require_once 'include/utils/utils.php';
-
 // danzi.tn#9 - 20170705 - custom function for project creation based on template
 
+
 function customProjectFromTemplate($entity){
+    include('dnz.config.php');
 	global $adb, $log;
+    
+
     
 	$id = $entity->data['id'];
 	$id_splitted = explode('x',$id);
@@ -16,10 +19,83 @@ function customProjectFromTemplate($entity){
     $log->debug("\tcustomProjectFromTemplate projectname=".$projectname);
 	
 	$projecttype = $entity->data['projecttype'];
-	if( $projecttype=='Coflyer') {
-	    $log->debug("customProjectFromTemplate: OK project type is Coflyer");
+	if( array_key_exists($projecttype, $dnzProjectTypeMap) ) {
+	    $projectName = $dnzProjectTypeMap[$projecttype];
+	    $log->debug("customProjectFromTemplate: OK project type is ".$projecttype. " associated to template ". $projectName);
+	    $projectres = $adb->pquery( 'SELECT projectid FROM vtiger_project JOIN vtiger_crmentity ON crmid=projectid AND deleted=0 WHERE projectname=?', array($projectName));
+	    if($adb->num_rows($projectres) > 0) {
+	    
+
+	    	$currentFocus = CRMEntity::getInstance('Project');
+		    $currentFocus->id = $projectId;
+		    $currentFocus->retrieve_entity_info($projectId, 'Project');
+		    
+    	    $templateId = $adb->query_result($projectres, 0, 'projectid');		    
+	    	$templateFocus = CRMEntity::getInstance('Project');
+		    $templateFocus->id = $projectId;
+		    $templateFocus->retrieve_entity_info($templateId, 'Project');
+		
+	        
+		    $column_names = array();
+    		foreach($templateFocus->column_fields as $fieldName=>$fieldValue) {
+	
+			        if( !empty($templateFocus->column_fields[$fieldName] ) &&
+			            empty($currentFocus->column_fields[$fieldName] ) && 
+			            ! in_array($fieldName, array('projectname','assigned_user_id','projecttype','createdtime','modifiedtime','linktoaccountscontacts') )  ) {
+				        // $currentRecordModel->set($fieldName, $templateFieldModel->getDBInsertValue($fieldValue));
+				        $currentFocus->column_fields[$fieldName] = $fieldValue;
+				        $log->debug("customProjectFromTemplate.templateFieldList field: ".$fieldName." set to " .$fieldValue);
+				        $column_names[$fieldName] = $fieldValue;
+			        } else {
+			            $log->debug("customProjectFromTemplate.templateFieldList non editable field: ".$fieldName);
+                    }
+			       		       
+		    }
+		    $log->debug("customProjectFromTemplate.templateFieldList column_names = ".print_r($column_names,True));
+		    $relatedQuery = "SELECT
+                                vtiger_crmentityrel.relcrmid,
+                                vtiger_crmentityrel.relmodule
+                                FROM
+                                vtiger_project
+                                JOIN
+                                vtiger_crmentity on vtiger_crmentity.crmid = vtiger_project.projectid AND vtiger_crmentity.deleted = 0
+                                JOIN
+                                vtiger_crmentityrel on vtiger_crmentityrel.crmid  = vtiger_project.projectid 
+                                WHERE
+                                vtiger_crmentityrel.relmodule IN ('ProjectTask','ProjectMilestone')
+                                AND vtiger_project.projectid = ?";
+		    $relatedRes = $adb->pquery($relatedQuery , array($templateId));
+    		$noOfRows = $adb->num_rows($relatedRes);
+
+		    for($i=0; $i<$noOfRows; ++$i) {
+			    $row = $adb->query_result_rowdata($relatedRes, $i);
+                $log->debug("customProjectFromTemplate.templateFieldList relatedRes: ".print_r($row,True));
+                $relatedFocus = CRMEntity::getInstance($row['relmodule']);
+		        $relatedFocus->id = $row['relcrmid'];
+		        $relatedFocus->retrieve_entity_info($row['relcrmid'], $row['relmodule']);
+		        
+		        $newRelated = CRMEntity::getInstance($row['relmodule']);
+	            vtlib_setup_modulevars($row['relmodule'],$newRelated);
+	            foreach($relatedFocus->column_fields as $fieldName=>$fieldValue) {
+	                $newRelated->column_fields[$fieldName] = $fieldValue;
+	            }
+	            $newRelated->column_fields['assigned_user_id'] = $currentFocus->column_fields['assigned_user_id'];
+		        $newRelated->column_fields['createdtime'] = $currentFocus->column_fields['createdtime'];
+        		$newRelated->column_fields['modifiedtime'] = $currentFocus->column_fields['modifiedtime'];
+        		$newRelated->column_fields['modifiedby'] = $currentFocus->column_fields['modifiedby'];
+        		$newRelated->column_fields['projectid'] = $projectId;
+        		$newRelated->save($module_name=$row['relmodule']);
+        		$currentFocus->save_related_module('Project',$projectId,$row['relmodule'],$newRelated->id);
+		    }
+		    
+	    	$currentFocus->mode = 'edit';
+		    $currentFocus->save($module_name='Project');
+		} else {
+    		$log->error("customProjectFromTemplate: project with project name ".$projectName. " does not exists!");
+		}
+	    
     } else {
-        $log->error("customProjectFromTemplate: KO project type != Coflyer - projecttype=".$projecttype);
+        $log->error("customProjectFromTemplate: KO project type ".$projecttype. " without template mapping");
     }
     $log->debug("customProjectFromTemplate terminated!");
 }
